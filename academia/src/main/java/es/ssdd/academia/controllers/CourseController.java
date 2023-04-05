03fa164fd47dab52834ea1c88ad7a21bf40a921a
Lanraxx <xxxenekoxxx@gmail.com>
2023-04-05 14:22:13 +0200

    update controllers

--- academia/src/main/java/es/ssdd/academia/controllers/CourseController.java
+++ academia/src/main/java/es/ssdd/academia/controllers/CourseController.java
@@ -7,6 +7,7 @@ import es.ssdd.academia.services.CommentService;
 import es.ssdd.academia.services.CourseService;
 import es.ssdd.academia.entities.Course;
 import es.ssdd.academia.services.ForumService;
+import es.ssdd.academia.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,23 +25,33 @@ public class CourseController {
     CourseService courseService;
     @Autowired
     ForumService forumService;
+    @Autowired
+    UserService userService;
 
     @PostConstruct
     public void CourseController () {
-        //Create users
-        User u1 = new User("Pepe", "pepejaun@gmail.com", "xxxxx");
-        User u2 = new User("Pepa", "junjwenc@yahoo.es", "12234");
-        ArrayList<User> list = new ArrayList<User>();
-        list.add(u1);
-        list.add(u2);
+        Course course1 = new Course("Introducción a Java", "Gratis", 10,
+                "Java es un lenguaje de programación orientado a objetos y, al mismo tiempo, una plataforma informática "
+                + "para el diseño y desarrollo de aplicaciones para distintos dispositivos."
+                + "Como curiosidad, te podemos contar que se trata de un sistema que se creó en 1995 y que tenía como "
+                + "objetivo ser un lenguaje de programación de estructura sencilla y que pudiese ser ejecutado en todos "
+                + "los sistemas operativos posibles. Java sigue siendo, tras más de 20 años en el mercado, "
+                + "uno de los lenguajes de programación más usados del mundo."
+                + "Son muchas las empresas que lo emplean para el desarrollo de tecnologías y aplicaciones en todo "
+                + "tipo de sistemas y dispositivos. Sin embargo, la cantidad de profesionales especializados en Java "
+                + "no ha crecido al mismo ritmo que su uso, por lo que los programadores y desarrolladores Java son "
+                + "perfiles muy cotizados.",
+                "https://www.oracle.com/oce/press/assets/CONT2F6AE229113D42EC9C59FAED5BAA0380/native/og-social-java-logo.gif");
 
-        //Create courses
-        Course course1 = new Course("CURSO PREDEFINIDO", "GRATUITO", 1, "bueno", list, "https://www.oracle.com/oce/press/assets/CONT2F6AE229113D42EC9C59FAED5BAA0380/native/og-social-java-logo.gif");
-        Course course2 = new Course("C2", "30", 4, "bueno", list, "/images/twitter_2.png");
+        Course course2 = new Course("C2", "30", 4, "bueno",
+                "/images/twitter_2.png");
+
+        course1.setUserList(userService.getUserListOfACourse(1));
+        course2.setUserList(userService.getUserListOfACourse(2));
+
         courseService.createCourse(course1);
         courseService.createCourse(course2);
 
-        //Create forum
         Forum forum1 = new Forum(course1.getId());
         forumService.createForum(forum1);
         course1.setFk_forum(forum1.getId());
@@ -55,15 +66,15 @@ public class CourseController {
         model.addAttribute("courses", courseService.getAll());
         return "listCourses";
     }
-    @GetMapping("/addCourse/")                      //Show form of add course
+    @GetMapping("/addCourse/")                      //Mostrar formulario añadir curso
     public String addCourse(Model model) {
         model.addAttribute("title", "FORMULARIO NUEVO CURSO");
         return "newCourse";
     }
-    @PostMapping("/courses/")                  //Save course
+    @PostMapping("/courses/")                  //Guardar un curso
     public String createCourse(Model model, @RequestParam String title, @RequestParam String price,
                                @RequestParam String description, @RequestParam int duration, @RequestParam String image) {
-        courseService.createCourse(new Course(title, price, duration, description, null, image));
+        courseService.createCourse(new Course(title, price, duration, description, image));
         model.addAttribute("courses", courseService.getAll());
         return "listCourses";
     }
@@ -83,17 +94,17 @@ public class CourseController {
         return "updateCourse";
     }
 
-    @PostMapping("/courses/{id}/edit/")                  //Save modify course
+    @PostMapping("/courses/{id}/edit/")                  //Guardar un curso modificado
     public String updateCourse(Model model, @PathVariable long id, @RequestParam String title, @RequestParam String price,
                                 @RequestParam String description, @RequestParam int duration, @RequestParam String image) {
 
-        Course courseModified = new Course(title, price, duration, description, null, image);
+        Course courseModified = new Course(title, price, duration, description, image);
         courseService.modifyCourse(id, courseModified);
         return "redirect:/courses/{id}/";
     }
 
-    @GetMapping("/courses/{id}/delete/")            //Delete course
-    public String deleteCourse(Model model, @PathVariable long id) {
+    @GetMapping("/courses/{id}/delete/")            //Borrar curso
+    public String deleteCourse(Model model, @PathVariable int id) {
         courseService.deleteCourse(id);
         return "redirect:/courses/";
     }
--- academia/src/main/java/es/ssdd/academia/controllers/UserController.java
+++ academia/src/main/java/es/ssdd/academia/controllers/UserController.java
@@ -1,22 +1,28 @@
 package es.ssdd.academia.controllers;
 
+import es.ssdd.academia.entities.Course;
 import es.ssdd.academia.entities.User;
+import es.ssdd.academia.services.CourseService;
 import es.ssdd.academia.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    CourseService courseService;
 
     @PostConstruct
     public void UserController () {
-        ArrayList<User> course1List = new ArrayList<>();
-        ArrayList<User> course2List = new ArrayList<>();
+
+        List<User> course1List = courseService.getOne(1).getUserList();
+        List<User> course2List = courseService.getOne(2).getUserList();
 
         User u1 = new User("Nico", "nrodriguezu@gmail.com", "xxxxx");
         User u2 = new User("Marta", "mmrtta@yahoo.es", "1234");
@@ -32,6 +38,30 @@ public class UserController {
         course2List.add(u5);
         course2List.add(u1);
         course2List.add(u3);
+
+        List<Course> enrolledCoursesUser1 = new ArrayList<>();
+        List<Course> enrolledCoursesUser2 = new ArrayList<>();
+        List<Course> enrolledCoursesUser3 = new ArrayList<>();
+        List<Course> enrolledCoursesUser4 = new ArrayList<>();
+        List<Course> enrolledCoursesUser5 = new ArrayList<>();
+
+        enrolledCoursesUser1.add(courseService.getOne(1));
+        enrolledCoursesUser1.add(courseService.getOne(2));
+
+        enrolledCoursesUser2.add(courseService.getOne(1));
+
+        enrolledCoursesUser3.add(courseService.getOne(1));
+        enrolledCoursesUser3.add(courseService.getOne(2));
+
+        enrolledCoursesUser4.add(courseService.getOne(2));
+
+        enrolledCoursesUser5.add(courseService.getOne(2));
+
+        u1.setEnrolledCourses(enrolledCoursesUser1);
+        u2.setEnrolledCourses(enrolledCoursesUser2);
+        u3.setEnrolledCourses(enrolledCoursesUser3);
+        u4.setEnrolledCourses(enrolledCoursesUser4);
+        u5.setEnrolledCourses(enrolledCoursesUser5);
 
         userService.createUser(u1);
         userService.createUser(u2);
