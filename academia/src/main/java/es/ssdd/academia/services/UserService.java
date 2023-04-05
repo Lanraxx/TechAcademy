8da0db0e09cd2e872e2b0ced702d95fce8c51452
Lanraxx <xxxenekoxxx@gmail.com>
2023-04-05 14:22:57 +0200

    Update UserService.java

--- academia/src/main/java/es/ssdd/academia/services/UserService.java
+++ academia/src/main/java/es/ssdd/academia/services/UserService.java
@@ -1,27 +1,44 @@
 package es.ssdd.academia.services;
 
+import es.ssdd.academia.entities.Course;
 import es.ssdd.academia.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class UserService {
+
     private Map<Long, User> usersMap = new ConcurrentHashMap<>();
     private AtomicLong id = new AtomicLong();
-
-
-    public UserService () {
-        this.createUser(new User("Pepe", "pepejaun@gmail.com", "xxxxx"));
-        this.createUser(new User("Pepa", "junjwenc@yahoo.es", "12234"));
-    }
 
     public User createUser(User user){
         long tem = id.incrementAndGet();
         user.setId(tem);
         usersMap.put(tem, user);
         return user;
+    }
+
+    public Collection<User> getAll() {
+        return usersMap.values();
+    }
+
+    public List<User> getUserListOfACourse(long id) {
+        List<User> userList = new ArrayList<>();
+        for (Map.Entry<Long, User> entry : usersMap.entrySet()) {
+            User user = entry.getValue();
+            for(int i = 0; i < 0; i++) {
+                if (id == user.getEnrolledCourses().get(i).getId()) {
+                    userList.add(user);
+                }
+            }
+        }
+        return userList;
     }
 }
