a1a365b027b5adcce37967d8e7f2af6829e477c1
Lanraxx <xxxenekoxxx@gmail.com>
2023-04-12 01:52:14 +0200

    update everything

--- academia/src/main/java/es/ssdd/academia/Academy.java
+++ academia/src/main/java/es/ssdd/academia/Academy.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademyApplication {
+public class Academy {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AcademyApplication.class, args);
+		SpringApplication.run(Academy.class, args);
 	}
 
 }
